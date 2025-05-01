07a5faa55c2cb093d4aa72a7c8889386b33425e5
Znu <cerauskastadas@gmail.com>
2025-05-01 19:48:33 +0300

    Update TRFR.lua

--- TRFR.lua
+++ TRFR.lua
@@ -1,6 +1,4 @@
-local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/main/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZnuSploit/Fluent/refs/heads/main/main.lua"))()
 
 local Window = Fluent:CreateWindow({
     Title = "The Rake: Fan Remake",
